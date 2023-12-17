149819610132e4ce86ab8eef8f0fcfcc751fc53c
corwinpro <corwinat@gmail.com>
2023-12-17 21:16:55 +0000

    DEV: enable control buttons

--- www/index.js
+++ www/index.js
@@ -52,7 +52,7 @@ const ALIVE_COLOR = "#000000";
 const checkElement = document.getElementById("sampleMethod");
 const canvas = document.getElementById("wasm-gol-canvas");
 const sizeInput = document.getElementById("sizeInputID");
-const universe = Universe.new();
+let universe = Universe.new();
 const ctx = canvas.getContext("2d");
 
 let animationId = null;
@@ -71,6 +71,85 @@ const pause = () => {
     animationId = null;
 };
 
+const bitIsSet = (n, arr) => {
+    const byte = Math.floor(n / 8);
+    const mask = 1 << n % 8;
+    return (arr[byte] & mask) === mask;
+};
+
+const drawGrid = (width, height) => {
+    ctx.beginPath();
+    ctx.strokeStyle = GRID_COLOR;
+
+    // Vertical lines.
+    for (let i = 0; i <= width; i++) {
+        ctx.moveTo(i * (CELL_SIZE + 1) + 1, 0);
+        ctx.lineTo(i * (CELL_SIZE + 1) + 1, (CELL_SIZE + 1) * height + 1);
+    }
+
+    // Horizontal lines.
+    for (let j = 0; j <= height; j++) {
+        ctx.moveTo(0, j * (CELL_SIZE + 1) + 1);
+        ctx.lineTo((CELL_SIZE + 1) * width + 1, j * (CELL_SIZE + 1) + 1);
+    }
+
+    ctx.stroke();
+};
+
+
+const getIndex = (row, column, width) => {
+    return row * width + column;
+};
+
+const drawCells = (universe) => {
+    const width = universe.width();
+    const height = universe.height()
+    const cellsPtr = universe.cells();
+    const cells = new Uint8Array(memory.buffer, cellsPtr, (width * height) / 8);
+
+    ctx.beginPath();
+
+    for (let row = 0; row < height; row++) {
+        for (let col = 0; col < width; col++) {
+            const idx = getIndex(row, col, width);
+
+            ctx.fillStyle = bitIsSet(idx, cells) ? ALIVE_COLOR : DEAD_COLOR;
+
+            ctx.fillRect(
+                col * (CELL_SIZE + 1) + 1,
+                row * (CELL_SIZE + 1) + 1,
+                CELL_SIZE,
+                CELL_SIZE,
+            );
+        }
+    }
+    ctx.stroke();
+};
+
+const tickSingle = (universe) => {
+    universe.tick();
+    drawGrid(universe.width(), universe.height());
+    drawCells(universe);
+};
+
+canvas.addEventListener("click", (event) => {
+    const boundingRect = canvas.getBoundingClientRect();
+
+    const scaleX = canvas.width / boundingRect.width;
+    const scaleY = canvas.height / boundingRect.height;
+
+    const canvasLeft = (event.clientX - boundingRect.left) * scaleX;
+    const canvasTop = (event.clientY - boundingRect.top) * scaleY;
+
+    const row = Math.min(Math.floor(canvasTop / (CELL_SIZE + 1)), universe.height() - 1);
+    const col = Math.min(Math.floor(canvasLeft / (CELL_SIZE + 1)), universe.width() - 1);
+
+    universe.toggle_cell(row, col);
+
+    drawGrid(universe.width(), universe.height());
+    drawCells(universe);
+});
+
 const handleCheckBoxChange = () => {
     const checked = checkElement.checked;
 
@@ -85,67 +164,9 @@ const handleCheckBoxChange = () => {
     canvas.height = (CELL_SIZE + 1) * height + 1;
     canvas.width = (CELL_SIZE + 1) * width + 1;
 
-    const drawGrid = () => {
-        ctx.beginPath();
-        ctx.strokeStyle = GRID_COLOR;
-
-        // Vertical lines.
-        for (let i = 0; i <= width; i++) {
-            ctx.moveTo(i * (CELL_SIZE + 1) + 1, 0);
-            ctx.lineTo(i * (CELL_SIZE + 1) + 1, (CELL_SIZE + 1) * height + 1);
-        }
-
-        // Horizontal lines.
-        for (let j = 0; j <= height; j++) {
-            ctx.moveTo(0, j * (CELL_SIZE + 1) + 1);
-            ctx.lineTo((CELL_SIZE + 1) * width + 1, j * (CELL_SIZE + 1) + 1);
-        }
-
-        ctx.stroke();
-    };
-
-    const getIndex = (row, column) => {
-        return row * width + column;
-    };
-
-    const bitIsSet = (n, arr) => {
-        const byte = Math.floor(n / 8);
-        const mask = 1 << n % 8;
-        return (arr[byte] & mask) === mask;
-    };
-
-    const drawCells = () => {
-        const cellsPtr = universe.cells();
-        const cells = new Uint8Array(memory.buffer, cellsPtr, (width * height) / 8);
-
-        ctx.beginPath();
-
-        for (let row = 0; row < height; row++) {
-            for (let col = 0; col < width; col++) {
-                const idx = getIndex(row, col);
-
-                ctx.fillStyle = bitIsSet(idx, cells) ? ALIVE_COLOR : DEAD_COLOR;
-
-                ctx.fillRect(
-                    col * (CELL_SIZE + 1) + 1,
-                    row * (CELL_SIZE + 1) + 1,
-                    CELL_SIZE,
-                    CELL_SIZE,
-                );
-            }
-        }
-        ctx.stroke();
-    };
-
-    const tickSingle = () => {
-        universe.tick();
-        drawGrid();
-        drawCells();
-    };
-
     const renderLoop = () => {
         fps.render();
-        tickSingle();
+        tickSingle(universe);
         animationId = requestAnimationFrame(renderLoop);
     };
 
@@ -156,7 +177,7 @@ const handleCheckBoxChange = () => {
 
     playNextButton.addEventListener("click", (event) => {
         if (isPaused()) {
-            tickSingle();
+            tickSingle(universe);
         }
     });
 
@@ -168,24 +189,30 @@ const handleCheckBoxChange = () => {
         }
     });
 
-    canvas.addEventListener("click", (event) => {
-        const boundingRect = canvas.getBoundingClientRect();
-
-        const scaleX = canvas.width / boundingRect.width;
-        const scaleY = canvas.height / boundingRect.height;
-
-        const canvasLeft = (event.clientX - boundingRect.left) * scaleX;
-        const canvasTop = (event.clientY - boundingRect.top) * scaleY;
-
-        const row = Math.min(Math.floor(canvasTop / (CELL_SIZE + 1)), height - 1);
-        const col = Math.min(Math.floor(canvasLeft / (CELL_SIZE + 1)), width - 1);
-
-        universe.toggle_cell(row, col);
-
-        drawGrid();
-        drawCells();
-    });
-
     play();
 };
 handleCheckBoxChange();
+
+checkElement.addEventListener("change", () => {
+    const checked = checkElement.checked;
+
+    universe.fill(
+        checked ? UniversePattern.Random : UniversePattern.Pattern,
+        sizeInput.valueAsNumber,
+        sizeInput.valueAsNumber,
+    );
+})
+sizeInput.addEventListener("input", () => {
+    const checked = checkElement.checked;
+
+    universe = Universe.new();
+    universe.fill(
+        checked ? UniversePattern.Random : UniversePattern.Pattern,
+        sizeInput.valueAsNumber,
+        sizeInput.valueAsNumber,
+    );
+
+    tickSingle(universe);
+    canvas.height = (CELL_SIZE + 1) * universe.height() + 1;
+    canvas.width = (CELL_SIZE + 1) * universe.width() + 1;
+})
